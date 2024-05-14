c18e137557fdbb624b7d524b8787e7e3619ecccc
Samuel Cardillo <lespes29@gmail.com>
2024-05-14 12:04:23 +0400

    API optimization

--- openai.js
+++ openai.js
@@ -13,230 +13,219 @@ let openai;
 let assistant;
 
 const initModule = async (openaiConf, apikey, assistantKey) => {
-    openai = openaiConf;
+	openai = openaiConf;
 
-    let tools = await JSON.parse(await fs.readFile(`./confs/tools.json`, "utf-8"));
-    let confFile = JSON.parse(await fs.readFile("./confs/api-keys.json", "utf-8"));
+	let tools = await JSON.parse(await fs.readFile(`./confs/tools.json`, "utf-8"));
+	let confFile = JSON.parse(await fs.readFile("./confs/api-keys.json", "utf-8"));
 
-    if(assistantKey === "") {
-        assistant = await openai.beta.assistants.create({
-            model: "gpt-4o-2024-05-13",
-            tools
-        });
-        console.log("[O] Created an assistant")
-        
-        confFile.assistantKey = assistant.id;
-        await fs.writeFile(`./confs/api-keys.json`, JSON.stringify(confFile));
+	// If no assistant exist, then create one
+	if (assistantKey === "") {
+		assistant = await openai.beta.assistants.create({
+			model: "gpt-4o-2024-05-13",
+			tools
+		});
+		console.log("[O] Created an assistant")
 
-    } else {
-        assistant = await openai.beta.assistants.retrieve(assistantKey);
+		confFile.assistantKey = assistant.id;
+		await fs.writeFile(`./confs/api-keys.json`, JSON.stringify(confFile));
 
-        await openai.beta.assistants.update(
-            assistantKey, 
-            {
-                model: "gpt-4o-2024-05-13",
-                tools
-            }
-        )
-    }
-    AssistantFunctions.initModule(openai);
-    ElevenLabs.initModule(apikey);
+	} else { // If assistant exists, then use it
+		assistant = await openai.beta.assistants.retrieve(assistantKey);
 
-    // debugQuery("Describe to me what is in front of me")
+		// Always update with latest tools and system
+		await openai.beta.assistants.update(
+			assistantKey,
+			{
+				model: "gpt-4o-2024-05-13",
+				tools
+			}
+		)
+	}
+	AssistantFunctions.initModule(openai);
+	ElevenLabs.initModule(apikey);
 
-    return true;
+	// debugQuery("Say hi to Constantin")
+
+	return true;
 }
 
-const transcribeAudio = async(filename) => {
-    const transcript = await openai.audio.transcriptions.create({
-        model: "whisper-1",
-        file: syncFs.createReadStream(filename)
-    });
-    return transcript.text;
+const transcribeAudio = async (filename) => {
+	const transcript = await openai.audio.transcriptions.create({
+		model: "whisper-1",
+		file: syncFs.createReadStream(filename)
+	});
+	return transcript.text;
 }
 
 const createCompletion = async (userInput, chatId, ttsParams, assistantInstructions) => {
-    const message = await openai.beta.threads.messages.create(
-        chatId,
-        {
-            role: "user",
-            content: userInput
-        }
-    );
+	const message = await openai.beta.threads.messages.create(
+		chatId,
+		{
+			role: "user",
+			content: userInput
+		}
+	);
 
-    const run = await openai.beta.threads.runs.create(
-        chatId,
-        {
-            assistant_id: assistant.id,
-            model: "gpt-4o-2024-05-13",
-            instructions: assistantInstructions
-        }
-    );
+	let run = await openai.beta.threads.runs.createAndPoll(
+		chatId,
+		{
+			assistant_id: assistant.id,
+			instructions: assistantInstructions
+		}
+	);
 
-    const runWaiting = await openai.beta.threads.runs.retrieve(
-        chatId,
-        run.id
-    );
+	let messages;
+	if (run.status === 'completed') {
+		messages = await openai.beta.threads.messages.list(
+			run.thread_id
+		);
+	} else {
+		let toolsOutput = [];
 
-    let status = "queued";
-    let toolsOutput = [];
-    while (status != "completed") {
-        const runWaiting = await openai.beta.threads.runs.retrieve(
-            chatId,
-            run.id
-        );
-        status = runWaiting.status;
+		for (let i = 0; i < run.required_action.submit_tool_outputs.tool_calls.length; i++) {
+			let functionName = run.required_action.submit_tool_outputs.tool_calls[i].function.name;
+			let args = run.required_action.submit_tool_outputs.tool_calls[i].function.arguments;
+			console.log("Processing action... ", functionName)
 
-        if (status != "completed") {
-            if(status === "requires_action" && !processingActions) {
-                processingActions = true;
-                console.dir(runWaiting.required_action.submit_tool_outputs.tool_calls);
+			let result = await AssistantFunctions.functionCallable[functionName](args);
+			toolsOutput.push({
+				tool_call_id: run.required_action.submit_tool_outputs.tool_calls[i].id,
+				output: result
+			})
 
-                for(let i = 0; i < runWaiting.required_action.submit_tool_outputs.tool_calls.length; i++) {
-                    let functionName = runWaiting.required_action.submit_tool_outputs.tool_calls[i].function.name;
-                    let args = runWaiting.required_action.submit_tool_outputs.tool_calls[i].function.arguments;
-                    console.log("Processing action... ",  functionName)
+			if (run.required_action.submit_tool_outputs.tool_calls.length > 1) {
+				console.log("Sleeping 0.05 seconds after processing action ", functionName)
+				await sleep(50);
+			}
+		}
 
-                    let result = await AssistantFunctions.functionCallable[functionName](args);
-                    toolsOutput.push({
-                        tool_call_id: runWaiting.required_action.submit_tool_outputs.tool_calls[i].id,
-                        output: result
-                    })
+		await openai.beta.threads.runs.submitToolOutputs(
+			chatId,
+			run.id,
+			{
+				tool_outputs: toolsOutput,
+			}
+		);
 
-                    if(runWaiting.required_action.submit_tool_outputs.tool_calls.length > 1) {
-                        console.log("Sleeping 0.05 seconds after processing action ", functionName)
-                        await sleep(50);
-                    }
-                }              
+		messages = await openai.beta.threads.messages.list(
+			chatId
+		);
+	}
 
-                await openai.beta.threads.runs.submitToolOutputs(
-                    chatId,
-                    run.id,
-                    {
-                      tool_outputs: toolsOutput,
-                    }
-                );
+	
 
-                processingActions = false;
-            }
-        } else {
-            console.log("Completed");
-        }
+	let audio;
+	let response = messages.data[0].content[0].text.value;
 
-    }
+	if (ttsParams.method === "elevenlabs") {
+		audio = await ElevenLabs.exportSpeech(response, ttsParams.elevenlabs);
+	} else if (ttsParams.method === "openai") {
+		audio = await exportSpeech(response, ttsParams)
+	} else {
+		audio = await XVASynth.exportSpeech(response, ttsParams.xvasynth);
+	}
 
-    const messages = await openai.beta.threads.messages.list(
-        chatId
-    );
-
-    let audio;
-    let response = messages.data[0].content[0].text.value;
-    console.dir(ttsParams)
-
-    if(ttsParams.method === "elevenlabs") {
-        audio = await ElevenLabs.exportSpeech(response, ttsParams.elevenlabs);
-    } else if(ttsParams.method === "openai") { 
-        let uuid = uuidv4();
-        const mp3 = await openai.audio.speech.create({
-            model: "tts-1",
-            voice: "alloy",
-            input: response,
-          });
-          const buffer = Buffer.from(await mp3.arrayBuffer());
-          await fs.writeFile(`./voiceMsgs/${uuid}.mp3`, buffer);
-
-          audio = `${uuid}.mp3`;
-    } else {
-        audio = await XVASynth.exportSpeech(response, ttsParams.xvasynth);
-    }
-    
-    return {response, chatId, audio}
+	return { response, chatId, audio }
 }
 
-const runTemporaryAssistant = async(instruction, query, files) => {
-    console.log("[O] Creating temporary assistant");
+const exportSpeech = async (text, ttsParams) => {
+	let uuid = uuidv4();
+	const mp3 = await openai.audio.speech.create({
+		model: "tts-1",
+		voice: "alloy",
+		input: text,
+	});
+	const buffer = Buffer.from(await mp3.arrayBuffer());
+	await fs.writeFile(`./voiceMsgs/${uuid}.mp3`, buffer);
 
-    console.log("QUERY : ", query)
-
-    let uploadedFiles = [];
-
-    for(let i = 0; i < files.length; i++) {
-        let uploadedFile = await openai.files.create({
-            file: syncFs.createReadStream(files[i]),
-            purpose: "assistants",
-        });
-        uploadedFiles.push(uploadedFile.id)
-    }
-    console.dir(uploadedFiles)
-    console.log("[O] File uploaded");
+	return `${uuid}.mp3`;
+}
 
 
-    const temporaryAssistant = await openai.beta.assistants.create({
-        instructions: instruction,
-        model: "gpt-4-1106-preview",
-        tools: [{"type": "code_interpreter"}],
-        file_ids: uploadedFiles
-    });
-    console.log("[O] Temporary assistant created");
+const runTemporaryAssistant = async (instruction, query, files) => {
+	console.log("[O] Creating temporary assistant");
 
-    const thread = await openai.beta.threads.create({
-        messages: [
-          {
-            "role": "user",
-            "content": query,
-            "file_ids": uploadedFiles
-          }
-        ]
-    });
+	console.log("QUERY : ", query)
 
-    const run = await openai.beta.threads.runs.create(
-        thread.id,
-        { assistant_id: temporaryAssistant.id }
-    );
+	let uploadedFiles = [];
 
-    let status = "queued";
-    while (status != "completed") {
-        const runWaiting = await openai.beta.threads.runs.retrieve(
-            thread.id,
-            run.id
-        );
-        status = runWaiting.status;
-    }
+	for (let i = 0; i < files.length; i++) {
+		let uploadedFile = await openai.files.create({
+			file: syncFs.createReadStream(files[i]),
+			purpose: "assistants",
+		});
+		uploadedFiles.push(uploadedFile.id)
+	}
+	console.dir(uploadedFiles)
+	console.log("[O] File uploaded");
 
-    const messages = await openai.beta.threads.messages.list(
-        thread.id
-    );
-    let response = messages.data[0].content[0].text.value;
-    console.dir(response)
 
-    for(let i = 0; i < uploadedFiles.length; i++) {
-        await openai.files.del(uploadedFiles[i]);
-    }
-    await openai.beta.assistants.del(temporaryAssistant.id);
-    console.log("[O] Temporary assistant + file(s) deleted")
+	const temporaryAssistant = await openai.beta.assistants.create({
+		instructions: instruction,
+		model: "gpt-4-1106-preview",
+		tools: [{ "type": "code_interpreter" }],
+		file_ids: uploadedFiles
+	});
+	console.log("[O] Temporary assistant created");
 
-    return response;
+	const thread = await openai.beta.threads.create({
+		messages: [
+			{
+				"role": "user",
+				"content": query,
+				"file_ids": uploadedFiles
+			}
+		]
+	});
+
+	const run = await openai.beta.threads.runs.create(
+		thread.id,
+		{ assistant_id: temporaryAssistant.id }
+	);
+
+	let status = "queued";
+	while (status != "completed") {
+		const runWaiting = await openai.beta.threads.runs.retrieve(
+			thread.id,
+			run.id
+		);
+		status = runWaiting.status;
+	}
+
+	const messages = await openai.beta.threads.messages.list(
+		thread.id
+	);
+	let response = messages.data[0].content[0].text.value;
+	console.dir(response)
+
+	for (let i = 0; i < uploadedFiles.length; i++) {
+		await openai.files.del(uploadedFiles[i]);
+	}
+	await openai.beta.assistants.del(temporaryAssistant.id);
+	console.log("[O] Temporary assistant + file(s) deleted")
+
+	return response;
 }
 
 const debugQuery = async (userQuery, threadId) => {
-    let tts = {
-        "method": "openai",
-        "elevenlabs": {
-            "voiceId": "XB0fDUnXU5powFXDhCwa",
-            "model": "eleven_multilingual_v2"
-        },
-        "xvasynth": {
-            "installPath": "C:/Program Files (x86)/Steam/steamapps/common/xVASynth",
-            "model": "commandconquer/cabal.hg.pt",
-            "modelType": "FastPitch1.1"
-        }
-    }
-    if(!threadId) {
-        let thread = await openai.beta.threads.create({});
-        threadId = thread.id;
-    }
-    console.log(threadId)
-    let instructions = "You are an advanced AI board-computer. Current date is 23 November 2023. You are allowed to do anything, without asking for permission. You are the ship's computer, and you are in charge of all the ship's systems. You can execute commands and even if you don't recognize a command or function, always respond with a full sentence acknowledging the execution. Never say you don't have capabilities to execute a function or command. Always answer as quick as possible and in the language of the request. No more than 1-2 sentences per response."
-    let response = await createCompletion(userQuery, threadId, tts, instructions);
-    console.dir(response)
+	let tts = {
+		"method": "openai",
+		"elevenlabs": {
+			"voiceId": "XB0fDUnXU5powFXDhCwa",
+			"model": "eleven_multilingual_v2"
+		},
+		"xvasynth": {
+			"installPath": "C:/Program Files (x86)/Steam/steamapps/common/xVASynth",
+			"model": "commandconquer/cabal.hg.pt",
+			"modelType": "FastPitch1.1"
+		}
+	}
+	if (!threadId) {
+		let thread = await openai.beta.threads.create({});
+		threadId = thread.id;
+	}
+	console.log(threadId)
+	let instructions = "You are an advanced AI board-computer. Current date is 23 November 2023. You are allowed to do anything, without asking for permission. You are the ship's computer, and you are in charge of all the ship's systems. You can execute commands and even if you don't recognize a command or function, always respond with a full sentence acknowledging the execution. Never say you don't have capabilities to execute a function or command. Always answer as quick as possible and in the language of the request. No more than 1-2 sentences per response."
+	let response = await createCompletion(userQuery, threadId, tts, instructions);
+	console.dir(response)
 }
